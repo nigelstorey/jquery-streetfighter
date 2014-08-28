69ed484094cf925c0a9165634b4855f1468c0bef
nigelstorey <nigelstorey@nigels-mbp.home>
2014-08-27 21:50:50 -0400

    hadouken working now

--- js/app.js
+++ js/app.js
@@ -4,7 +4,32 @@ $(document).ready(function() {
     $('.ryu-ready').show();
   })
   .mouseleave(function() {
- 	 $('.ryu-ready').hide();
-	$('.ryu-still').show();
+    $('.ryu-ready').hide();
+    $('.ryu-still').show();
+  })
+  .mousedown(function() {
+    playHadouken();    
+    $('.ryu-ready').hide();
+    $('.ryu-throwing').show();
+    $('.hadouken').finish().show()
+   
+    .animate(
+      {'left': '300px'},
+      500,
+      function() {
+        $(this).hide();
+        $(this).css('left', '-212px');
+      }
+    );
+  })
+  .mouseup(function() {
+    $('.ryu-throwing').hide();
+    $('.ryu-ready').show();
   });
-})
+});
+
+function playHadouken () {
+  $('#hadouken-sound')[0].volume = 0.5;
+  $('#hadouken-sound')[0].load();
+  $('#hadouken-sound')[0].play();
+}
